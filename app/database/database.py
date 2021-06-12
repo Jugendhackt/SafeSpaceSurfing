ae136199eaacfe45939fd70c3173944a3091d6d5
MacaroniDamage <daniel.tohmas@hotmail.com>
2021-06-12 18:05:24 +0200

    default facilities will be added to the database

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
-from .models import db
+from .models import db, Facility
+import json
 
 def init_app(app):
+
+    #passes the app(flask object) to the sql alchemey library
     db.init_app(app)
+
+    #Generates the Database models
     db.create_all(app=app)
+
+    #parses data from the default_facilities file and 
+    #adds the information into the database
+    parse_and_pull_data(app)
+
+def parse_and_pull_data(app):
+    with app.app_context():
+        jsonFile = open("database/default_facilities.json", "rt")
+        json_data = json.load(jsonFile)
+        for item in json_data["Facilities"]:
+            facility = Facility(item)
+            db.session.add(facility)
+        db.session.commit()
+        print(str(len(json_data['Facilities'])) + " facilities added to the database")
+
+
+
--- app/database/models.py
+++ app/database/models.py
@@ -3,23 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class Facilities(db.Model):
-    __tablename__ = 'facilities'
+class Facility(db.Model):
+    __tablename__ = 'facility'
     f_id = db.Column(db.Integer, primary_key=True)
-    osm_id = db.Column(db.String())
-    geolocation = db.Column(db.String())
-    name = db.Column(db.String(), nullable=False)
-    description = db.Column(db.String())
-    owner_id = db.Column(db.Integer, db.ForeignKey('users.u_id'), nullable=False)
-    owner = db.relationship('Facilities', backref=db.backref('users', lazy=True))
+    osm_id = db.Column(db.String(100))
+    geolocation = db.Column(db.String(1000))
+    name = db.Column(db.String(1000))
+    description = db.Column(db.String(1000))
+    #owner_id = db.Column(db.Integer, db.ForeignKey('users.u_id'), nullable=False)
+    #owner = db.relationship('Facilities', backref=db.backref('users', lazy=True))
+
+    def __init__(self, osm_id, **kwargs):
+        super().__init__(**kwargs)
+        self.osm_id = osm_id
+
 
     def __repr__(self):
-        return '<Playlistname %r>' % self.name
+        return '<Facility %r>' % self.osm_id
 
 
 
-class Users(db.Model):
-    __tablename__ = 'users'
+class User(db.Model):
+    __tablename__ = 'user'
     #Table attributes
     u_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), nullable=False)
@@ -27,4 +32,4 @@ class Users(db.Model):
     is_admin = db.Column(db.Boolean(), nullable=False)
 
     def __repr__(self):
-        return '<Titlename %r>' % self.name
+        return '<User %r>' % self.name
